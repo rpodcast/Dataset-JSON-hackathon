f3757039d9dd24dac8d077ddc40fb4a61f44f077
Eric Nantz <theRcast@gmail.com>
2025-03-14 23:28:08 -0400

    feat: add custom function for exporting to shinylive

---
+++ export_to_shinylive.R
@@ -0,0 +1,42 @@
+library(shinylive)
+
+build_app <- function(dir_build = "site", overwrite = TRUE, verbose = TRUE) {
+  # copy current app files to a temporary directory
+  if (isFALSE(overwrite) && fs::dir_exists(dir_build)) {
+    withr::with_options(
+      list(rlang_backtrace_on_error = "none"),
+      cli::cli_abort(
+        c("Output directory {dir_build} already exists.",
+          "Delete the directory or set {.code overwrite=TRUE}"
+        ),
+        call = NULL
+      )
+    )
+  }
+
+
+  temp_dir <- fs::file_temp("shinyapp-project-dir")
+  on.exit(fs::dir_delete(temp_dir))
+
+  temp_dir_source <- fs::path_join(c(temp_dir, "src"))
+  temp_dir_source_www <- fs::path_join(c(temp_dir_source, "www"))
+  temp_dir_out <- fs::path_join(c(temp_dir, "out"))
+
+  fs::dir_create(temp_dir)
+  fs::dir_create(temp_dir_source_www, recurse = TRUE)
+  fs::dir_create(temp_dir_out, recurse = TRUE)
+  fs::file_copy("app.R", fs::path(temp_dir_source, "app.R"))
+  fs::dir_copy("R", fs::path(temp_dir_source, "R"))
+  fs::dir_copy("www", fs::path(temp_dir_source))
+
+  shinylive::export(
+    temp_dir_source,
+    temp_dir_out,
+    verbose = verbose,
+    wasm_packages = TRUE
+  )
+
+  fs::dir_copy(temp_dir_out, dir_build)
+}
+
+build_app()
